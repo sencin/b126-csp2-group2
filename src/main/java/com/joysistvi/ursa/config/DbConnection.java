60af712516c8d8e0cd1e1500a903f22157fca3dc
sencin <asunayuk34@gmail.com>
2026-07-02 16:56:29 +0800

    removed using hikari driver

--- src/main/java/com/joysistvi/ursa/config/DbConnection.java
+++ src/main/java/com/joysistvi/ursa/config/DbConnection.java
@@ -1,52 +1,38 @@
 package com.joysistvi.ursa.config;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-    private static class Holder {
-        private static final DbConnection INSTANCE = new DbConnection();
-    }
 
-    private final HikariDataSource dataSource;
+    private static final String URL = "jdbc:mysql://localhost:3306/ursa_db";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private static final DbConnection INSTANCE = new DbConnection();
 
     private DbConnection() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/ursa_db");
-        config.setUsername("root");
-        config.setPassword("root");
-
-        config.setMaximumPoolSize(5);
-        config.setMinimumIdle(2);
-        config.setConnectionTimeout(30000);
-        config.setIdleTimeout(600000);
-
-        this.dataSource = new HikariDataSource(config);
-    }
-    public static DbConnection getInstance() {
-        return Holder.INSTANCE;
-    }
-
-    public Connection getConnection() throws SQLException {
-        return this.dataSource.getConnection();
-    }
-
-    public void closePool() {
-        if (this.dataSource != null && !this.dataSource.isClosed()) {
-            this.dataSource.close();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MySQL JDBC Driver not found.", e);
         }
     }
 
+    public static DbConnection getInstance() {
+        return INSTANCE;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+
     public static void init() {
-        try {
-            getInstance().getConnection().close();
+        try (Connection connection = getInstance().getConnection()) {
             System.out.println("Database subsystem successfully initialized and verified.");
         } catch (SQLException e) {
             throw new RuntimeException("Could not connect to the database!", e);
         }
     }
-
 }
